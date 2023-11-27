d45e42c4262a7b6b71af757541bd4ad722cd2b0e
beshik7 <tommypdx7@gmail.com>
2023-11-27 01:59:22 -0800

    Practice task

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.List;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,6 @@
+import java.util.Objects;
+
+public class User {
+}
+
+
